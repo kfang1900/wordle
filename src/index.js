61d87ec2fa4aee7c5d505bedf31ec255f3349196
Kevin Fang <kevinfang@Kevins-MacBook-Pro.local>
2025-03-13 12:28:34 +0900

    fix redirect agaain

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles.css';
 
-import App from "./App";
+import App from './App';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <Router basename="/wordle">
+      <App />
+    </Router>
   </StrictMode>
 );
